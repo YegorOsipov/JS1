932add899c70debad098174f82c1c8e9d9d78a00
Egor <gogsyan@gmail.com>
2021-06-05 21:01:39 +0300

    2 подзадание

--- script.js
+++ script.js
@@ -23,3 +23,12 @@
 
 'use strict';
 
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
